e7cea889649e83ed5714212608f2de2a5a7edb76
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-22 20:26:21 +0300

    feat: basic implementation

--- src/dev/zhelezov/jsonserializer/App.java
+++ src/dev/zhelezov/jsonserializer/App.java
@@ -1,7 +1,50 @@
 package dev.zhelezov.jsonserializer;
 
+import java.lang.reflect.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        StringBuilder sb = new StringBuilder();
+
+        User user = new User(1, null, true, "password", "username");  // Example object
+        Class<?> cls = user.getClass();
+
+        sb.append("{\n\"");
+        sb.append(cls.getSimpleName());
+        sb.append("\": {\n");
+
+        Field fieldList[] = cls.getDeclaredFields();
+
+        for (int i = 0; i < fieldList.length; i++) {
+            boolean isLastIteration = (i == fieldList.length - 1);
+
+            Field field = fieldList[i];
+
+            field.setAccessible(true);
+
+            sb.append("\"");
+            sb.append(field.getName());
+            sb.append("\": ");
+
+            Object fieldValue = field.get(user);
+
+            if (field.getType() == String.class && fieldValue != null) {
+                sb.append("\"");
+                sb.append(fieldValue);
+                sb.append("\"");
+            } else {
+                sb.append(fieldValue);
+            }
+            if (!isLastIteration) {
+                sb.append(",");
+            }
+            sb.append("\n");
+
+            field.setAccessible(false);
+        }
+
+        sb.append("}\n}");
+
+        System.out.println(sb.toString());
     }
 }
--- src/dev/zhelezov/jsonserializer/User.java
+++ src/dev/zhelezov/jsonserializer/User.java
@@ -4,29 +4,54 @@ public class User {
     private String username;
     private String email;
     private String password;
-    
-    public User(String username, String email, String password) {
-        this.username = username;
+    private boolean isAdmin;
+    private int age;
+
+    public User(int age, String email, boolean isAdmin, String password, String username) {
+        this.age = age;
         this.email = email;
+        this.isAdmin = isAdmin;
         this.password = password;
+        this.username = username;
     }
-    
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public boolean isIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
